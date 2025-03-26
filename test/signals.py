f21d8cf26cad6e0c0368a819335c0970eac12704
dauxanhduamuoi <dauxanhduamuoiabc123@gmail.com>
2025-03-26 08:15:24 +0700

    Lan3

--- test/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import Customer_User
-
-@receiver(post_save, sender=User)
-def create_customer(sender, instance, created, **kwargs):
-    if created:
-        Customer_User.objects.create(user=instance)
